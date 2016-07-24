0df770e3bc90690e4b640c975b50a5c1345c97c2
Esdras Barreto <esdras.barreto@gmail.com>
2016-07-24 19:11:12 -0300

    Fixed dependencies Fixed camel call to logger and logback.xml

--- lista-compras-consumer/src/main/java/org/sample/compras/consumer/camel/route/ControllerRoute.java
+++ lista-compras-consumer/src/main/java/org/sample/compras/consumer/camel/route/ControllerRoute.java
@@ -2,14 +2,15 @@ package org.sample.compras.consumer.camel.route;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.sql.SqlComponent;
 import org.sample.compras.consumer.util.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import javax.sql.DataSource;
 
 import static org.apache.activemq.camel.component.ActiveMQComponent.activeMQComponent;
 
@@ -18,6 +19,8 @@ import static org.apache.activemq.camel.component.ActiveMQComponent.activeMQComp
  */
 @Component
 public class ControllerRoute extends RouteBuilder {
+
+    private static final Logger CAMEL_LOGGER = LoggerFactory.getLogger("camel");
 
     @Autowired
     private Constants constants;
@@ -31,17 +34,16 @@ public class ControllerRoute extends RouteBuilder {
         camelContext.addComponent("sql", new SqlComponent(camelContext));
 
         from("activemq:queue:" + constants.getQueueName())
-                .log("Received message [${body}]")
+                .log(LoggingLevel.INFO, CAMEL_LOGGER, "Received message [${body}]")
                 .to("sql:insert into item (id, amount) values (:#id, :#amount)?dataSourceRef=dataSource")
                 .to("sql:select * from item?dataSourceRef=dataSource")
                 .process(new Processor() {
-
                     public void process(Exchange exchange)
                             throws Exception {
-                        System.out.println(exchange.getIn()
-                                .getBody().getClass());
-                        System.out.println(exchange.getIn()
-                                .getBody());
+                        CAMEL_LOGGER.info(exchange.getIn()
+                                .getBody().getClass().toString());
+                        CAMEL_LOGGER.info(exchange.getIn()
+                                .getBody().toString());
                     }
                 })
                 .end();
--- lista-compras-producer/src/main/java/org/sample/compras/producer/camel/route/ControllerRoute.java
+++ lista-compras-producer/src/main/java/org/sample/compras/producer/camel/route/ControllerRoute.java
@@ -1,10 +1,13 @@
 package org.sample.compras.producer.camel.route;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.sample.compras.producer.camel.processor.ItemProcessor;
 import org.sample.compras.producer.util.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +18,8 @@ import static org.apache.activemq.camel.component.ActiveMQComponent.activeMQComp
  */
 @Component
 public class ControllerRoute extends RouteBuilder {
+
+    private static final Logger CAMEL_LOGGER = LoggerFactory.getLogger("camel");
 
     @Autowired
     private ItemProcessor itemProcessor;
@@ -33,12 +38,12 @@ public class ControllerRoute extends RouteBuilder {
                 .bindingMode(RestBindingMode.auto);
 
         rest("/")
-                .get("ping").produces("text/html").route().transform().constant("is alive").log("Received ping [${body}]").endRest()
+                .get("ping").produces("text/html").route().transform().constant("is alive").log(LoggingLevel.INFO, CAMEL_LOGGER, "Received ping [${body}]").endRest()
                 .get("/add/{id}/{amount}").produces("text/html").to("direct:add");
 
         from("direct:add")
                 .process(itemProcessor)
-                .log("Received message [${body}]")
+                .log(LoggingLevel.INFO, CAMEL_LOGGER, "Received message [${body}]")
                 .inOnly("activemq:queue:" + constants.getQueueName())
                 .transform().constant("item added").end();
     }
