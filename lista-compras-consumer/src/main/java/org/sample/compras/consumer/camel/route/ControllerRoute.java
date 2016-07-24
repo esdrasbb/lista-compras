0eed3ebdf09e04496cbdf7935c96eda07eb8d4b4
Esdras Barreto <esdras.barreto@gmail.com>
2016-07-23 23:33:25 -0300

    Added hsqldb Added sql camel component

--- lista-compras-consumer/src/main/java/org/sample/compras/consumer/camel/route/ControllerRoute.java
+++ lista-compras-consumer/src/main/java/org/sample/compras/consumer/camel/route/ControllerRoute.java
@@ -1,10 +1,15 @@
 package org.sample.compras.consumer.camel.route;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.sql.SqlComponent;
 import org.sample.compras.consumer.util.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
 
 import static org.apache.activemq.camel.component.ActiveMQComponent.activeMQComponent;
 
@@ -23,9 +28,22 @@ public class ControllerRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         camelContext.addComponent("activemq", activeMQComponent(constants.getActiveMQBrokerUrl()));
+        camelContext.addComponent("sql", new SqlComponent(camelContext));
 
         from("activemq:queue:" + constants.getQueueName())
                 .log("Received message [${body}]")
+                .to("sql:insert into item (id, amount) values (:#id, :#amount)?dataSourceRef=dataSource")
+                .to("sql:select * from item?dataSourceRef=dataSource")
+                .process(new Processor() {
+
+                    public void process(Exchange exchange)
+                            throws Exception {
+                        System.out.println(exchange.getIn()
+                                .getBody().getClass());
+                        System.out.println(exchange.getIn()
+                                .getBody());
+                    }
+                })
                 .end();
     }
 }
---
+++ lista-compras-consumer/src/main/resources/schema-hsqldb.sql
@@ -0,0 +1,4 @@
+CREATE TABLE item(
+  id          BIGINT,
+  amount      INTEGER
+);
